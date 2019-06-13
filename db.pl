44b648bc9e4d9abaed30621c8823d89712838c57
Jambo <jambotoyou@gmail.com>
2019-06-13 06:01:26 +0400

    1

--- db.pl
+++ db.pl
@@ -3,9 +3,11 @@ record(1,"asdf").
 record(10,"Great ! you are faggot ! Congratulations gentleman").
 record(9,"Mutterland").
 record(6,"Reducto!").
-question(4,".",[],[]).
-question(1,"Test question",[["answer1",2]],[]).
-question(3,"Another question",[["answer3",6]],[]).
+question(1,"Важна ли цена?",[],[]).
 question(2,"Some question",[["grh",5],[".",4],["answer2",3]],[]).
-question(5,"Have you ever been to France ?",[],[["Yes",10]]).
+question(3,"Another question",[["answer3",6]],[]).
+question(4,".",[],[]).
+question(6,"AM I TYU ?",[],[]).
+question(1,"Test question",[["tyu",6],["answer1",2]],[]).
+attribute(10, "Material", "Stone").
 root(1).
